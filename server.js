8707a2ba44b13fb444acaba7901f57e8627a45a3
LzhJack <zhuohan9459@gmail.com>
2022-05-13 21:45:06 -0700

    Added cors for security bypass.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.use(express.static(__dirname ));
+app.use(cors());
+
+app.use(express.static(__dirname));
 
 app.listen(process.env.PORT || 5005, function (err) {
     if (err)
         console.log(err);
 });
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendFile('poke.html', { root: __dirname });
 });
