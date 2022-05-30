fa5d177d5f9a826205ea4426440f0a25f8b0e675
LzhJack <zhuohan9459@gmail.com>
2022-05-29 20:45:08 -0700

    Added img directory on server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 app.use("/css", express.static("./css"));
 app.use("/js", express.static("./js"));
-// app.use("/img", express.static("./img"))
+app.use("/img", express.static("./img"))
 
 
 app.get("/", function (req, res) {
